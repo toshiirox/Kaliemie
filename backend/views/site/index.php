6b89e93fe75547c76c06c5ac4349901030b4f285
Corentin Chateau <toshiirox@gmail.com>
2015-03-18 17:10:23 +0100

    Original

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,51 +1,42 @@
 <?php
 /* @var $this yii\web\View */
-$this->title = 'My Yii Application';
+$this->title = 'Gestion du site';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Gestion des données </h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">Vous êtes bien sur le module de gestion du site.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href='index.php?r=site%2Flogin'>Se Connecter et gérer le site.</a></p>
     </div>
+    <?php
+    if (Yii::$app->user->isGuest) {
 
-    <div class="body-content">
+    } else {
+        ?>
+        <div class="body-content">
+            <div class="row">
+                <div class="col-lg-4">
+                    <h2>Gestion des Patients</h2>
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+                    <p><a class="btn btn-default" href="#">Patients &raquo;</a></p>
+                </div>
+                <div class="col-lg-4">
+                    <h2>Gestion des Patients</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                    <p><a class="btn btn-default" href="#">Patients &raquo;</a></p>
+                </div>
+                <div class="col-lg-4">
+                    <h2>Gestion des <Patient></Patient>s</h2>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                    <p><a class="btn btn-default" href="#">Patients &raquo;</a></p>
+                </div>
             </div>
         </div>
+    <?php
+    }
 
-    </div>
+    ?>
 </div>
