96be1e4520c5efad2fe6543ff4ad81e2100b462e
Corentin Chateau <toshiirox@gmail.com>
2015-03-18 16:32:33 +0100

    Original

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -27,7 +27,7 @@ AppAsset::register($this);
         <?php
             NavBar::begin([
                 'brandLabel' => 'Kaliémie',
-                'brandUrl' => Yii::$app->homeUrl,
+                'brandUrl' => 'http://www.akiyoshi.fr/Kaliemie/frontend/',
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
                 ],
@@ -35,8 +35,23 @@ AppAsset::register($this);
             echo Nav::widget([
                 'options' => ['class' => 'navbar-nav navbar-right'],
                 'items' => [
-                    ['label' => 'Home', 'url' => ['/site/index']],
-                    ['label' => 'About', 'url' => ['/site/about']],
+                    Yii::$app->user->isGuest ?
+                        ['label' => 'Patients', 'url' => ['/site/login']] :
+                    ['label' => 'Patients', 'url' => ['/patients/index']],
+                    Yii::$app->user->isGuest ?
+                        ['label' => 'Actes', 'url' => ['/site/login']] :
+                    ['label' => 'Actes', 'url' => ['/actes/index']],
+                    Yii::$app->user->isGuest ?
+                        ['label' => 'Actes/Visites', 'url' => ['/site/login']] :
+                    ['label' => 'Actes/Visites', 'url' => ['/actes-visites/index']],
+                    Yii::$app->user->isGuest ?
+                        ['label' => 'Capacitées', 'url' => ['/site/login']] :
+                    ['label' => 'Capacitées', 'url' => ['/capacite/index']],
+                    Yii::$app->user->isGuest ?
+                        ['label' => 'Visites', 'url' => ['/site/login']] :
+                    ['label' => 'Visites', 'url' => ['/visites/index']],
+                    Yii::$app->user->isGuest ?
+                        ['label' => 'Contact', 'url' => ['/site/login']] :
                     ['label' => 'Contact', 'url' => ['/site/contact']],
                     Yii::$app->user->isGuest ?
                         ['label' => 'Login', 'url' => ['/site/login']] :
