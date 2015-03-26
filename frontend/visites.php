f0001eea766625724675eab17579eda664636e79
galissiaz <galissiaz.rudy@gmail.com>
2015-03-26 15:24:09 +0100

    requete fonctionnelle, voir mise en forme du tableau( repetition des dates début et fin pour une meme visite)

--- frontend/visites.php
+++ frontend/visites.php
@@ -8,31 +8,77 @@ $resultat = $req->fetchAll();
     <table>
     <caption>Interventions</caption>
     <tr>
-        <th>Nom du client :</th>
-        <th>Intervention(s) :</th>
-        <th>Durée :</th>
-        <th>Date début :</th>
-        <th>Date fin :</th>
+        <th>Nom du client : </th>
+        <th> Intervention(s) : </th>
+        <th> Durée : </th>
+        <th> Date début : </th>
+        <th> Date fin : </th>
     </tr>
 <?php
 if (!$resultat) {
     print 'Ta requete marche pas';
     ?>
     </table>
-    <?php
+<?php
 }
 else {
-
+    $voidname="";
+    $voiddatedeb="";
+    $voiddatefin="";
     foreach ($resultat as $r) {
+
         ?>
-
-
         <tr>
-            <td><?php echo $r["nom"]; ?></td>
-            <td><?php echo $r["libellee"]; ?></td>
+            <td>
+                <?php
+
+                if($voidname=="") {
+                    echo $r["nom"];
+                    $voidname=$r["nom"];
+                }
+
+                elseif($voidname!=$r["nom"]){
+                    echo $r["nom"];
+                    $voidname=$r["nom"];
+                }
+
+                ?>
+            </td>
+
+            <td><?php echo utf8_encode($r["libellee"]); ?></td>
+
             <td><?php echo $r["duree"]; ?></td>
-            <td><?php echo $r["DateDebut"]; ?></td>
-            <td><?php echo $r["DateFin"]; ?></td>
+
+            <td>
+                <?php
+
+                if ($voiddatedeb="") {
+                    echo $r["DateDebut"];
+                    $voiddatedeb = $r["DateDebut"];
+                }
+
+                elseif($voiddatedeb!=$r["DateDebut"]){
+                    echo $r["DateDebut"];
+                    $voiddatedeb=$r["DateDebut"];
+                }
+                ?>
+            </td>
+
+            <td>
+                <?php
+
+                if ($voiddatefin="") {
+                    echo $r["DateFin"];
+                    $voiddatefin = $r["DateFin"];
+                }
+
+                elseif($voiddatefin!=$r["DateFin"]){
+                    echo $r["DateFin"];
+                    $voiddatefin=$r["DateFin"];
+                }
+                ?>
+            </td>
+
         </tr>
     <?php
     }
